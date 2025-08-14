19b93a9ae8f091df8f9f3035342508ea278c8501
Matheus Eliaquim <202507364367@alunos.estacio.br>
2025-08-14 16:40:20 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -43,7 +43,30 @@ int main () {
     tabuleiro[v][h] = '3';
     }
 
+    //habilidade 01 - cone
+   int dv = 7;
+   int dh = 0;
+    for (v = 0; v < 3; v++) {
+        for (h = 5 - v; h <= 5 + v; h++){        
+            tabuleiro[v + dv][h + dh] = '5';
+        }   
+    }
 
+    //habilidade 02 - cruz  
+    for (v = 0; v < 3; v++){
+    tabuleiro[v][7] = '5';
+    } 
+    for (h = 5; h < 10; h++){
+    tabuleiro[1][h] = '5';
+    }
+
+    //habilidade 03 - octaedro  
+    for (v = 3; v < 6; v++){
+    tabuleiro[v][5] = '5';
+    } 
+    for (h = 4; h < 7; h++){
+    tabuleiro[4][h] = '5';
+    }
 
     //exibição do indice de linhas e exibição das linhas
     for (v = 0; v < 10; v++){
